8732fd96d4c3fd4771be8e48b588390fed0b03dd
Jjiya <gpwl9610@naver.com>
2022-05-24 14:25:16 +0900

    add Main class

--- baekjoon/src/com/codingTest/baekjoon/Main.java
+++ baekjoon/src/com/codingTest/baekjoon/Main.java
@@ -1,12 +1,6 @@
 package com.codingTest.baekjoon;
 
-import com.codingTest.baekjoon.bronze.B_10000;
-
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        B_10000 test = new B_10000();
-        test.test();
+    public static void main(String[] args) {
     }
 }
