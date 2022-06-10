11f37ddf4a5b4e22eb71a18bc3c2afe0f26c8ac3
Jjiya <gpwl9610@naver.com>
2022-06-11 01:48:02 +0900

    basicMath2 in silver 11653 / Solving silver 1929 in basicMath2

--- baekjoon/src/com/codingTest/baekjoon/Main.java
+++ baekjoon/src/com/codingTest/baekjoon/Main.java
@@ -1,13 +1,13 @@
 package com.codingTest.baekjoon;
 
 
-import com.codingTest.baekjoon.silver.basicMath2.S_2581;
+import com.codingTest.baekjoon.silver.basicMath2.S_11653;
 
 import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    S_2581 test = new S_2581();
+    S_11653 test = new S_11653();
     test.test();
   }
 }
