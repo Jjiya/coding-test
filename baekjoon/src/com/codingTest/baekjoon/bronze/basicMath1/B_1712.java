937fd35e929ecf54ba476bb66c36d711fab306e7
Jjiya <gpwl9610@naver.com>
2022-06-11 01:13:51 +0900

    Participate in the gahui test at 2022/06/06

--- baekjoon/src/com/codingTest/baekjoon/bronze/basicMath1/B_1712.java
+++ baekjoon/src/com/codingTest/baekjoon/bronze/basicMath1/B_1712.java
@@ -7,21 +7,33 @@ public class B_1712 {
   public static void test() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
+    double a = Double.parseDouble(st.nextToken());
+    double b = Double.parseDouble(st.nextToken());
+    double c = Double.parseDouble(st.nextToken());
 
-    int a = Integer.parseInt(st.nextToken());
-    int b = Integer.parseInt(st.nextToken());
-    int c = Integer.parseInt(st.nextToken());
-
-    int sales1 = (c - (a + b));
-    if (0 < sales1) {
-      System.out.print(1);
+    int sales = 1;
+    boolean possible = false;
+    while (sales <= 100) {
+      if (b * sales < c * sales) {
+        possible = true;
+        break;
+      }
+      sales++;
+    }
+    if (!possible) {
+      System.out.print(-1);
       return;
     }
 
-    int sales2 = (c * 2 - (a + (b * 2)));
+    int interval = 10 * ((a + "").length() - (c + "").length());
+    while (interval < a) {
+      if (a + (b * sales) < c * sales * interval) {
+        break;
+      } else {
+        interval *= 10;
+      }
+    }
 
-    System.out.print(sales1 < sales2 ? (sales1 / (sales1 - sales2)) + 2 : -1);
-
-//    다른 답안 보니까 더 간단함 >> (A / (C - B) + 1)
+    System.out.print(interval + sales);
   }
 }
---
+++ baekjoon/src/com/codingTest/baekjoon/gahuiTest/A.java
@@ -0,0 +1,18 @@
+package com.codingTest.baekjoon.gahuiTest;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class A {
+  public static void test() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    double a = Integer.parseInt(st.nextToken());
+    double b = Double.parseDouble(st.nextToken());
+
+    double earnedRunAverage = a - (a * (1.0 - (b / 100)));
+
+    System.out.print(100 <= a - earnedRunAverage ? 0 : 1);
+  }
+}
