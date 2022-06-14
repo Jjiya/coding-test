0b866f9ed4ed4c9507c188a968788b8d837a031c
Jjiya <gpwl9610@naver.com>
2022-06-14 17:47:19 +0900

    basicMath2 silver 4948 / try to 4948 using 포함배제의 원리

--- baekjoon/src/com/codingTest/baekjoon/Main.java
+++ baekjoon/src/com/codingTest/baekjoon/Main.java
@@ -1,12 +1,13 @@
 package com.codingTest.baekjoon;
 
-import com.codingTest.baekjoon.silver.basicMath1.S_2839;
+
+import com.codingTest.baekjoon.silver.basicMath2.S_4948;
 
 import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    S_2839 test = new S_2839();
+    S_4948 test = new S_4948();
     test.test();
   }
 }
