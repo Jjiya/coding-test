d532e9a9c8da4d982d35e2f41b8eb2a15387df45
Jjiya <gpwl9610@naver.com>
2022-06-15 09:45:19 +0900

    basicMath2 complete with silver 9020

--- baekjoon/src/com/codingTest/baekjoon/Main.java
+++ baekjoon/src/com/codingTest/baekjoon/Main.java
@@ -1,13 +1,13 @@
 package com.codingTest.baekjoon;
 
 
-import com.codingTest.baekjoon.silver.basicMath2.S_4948;
+import com.codingTest.baekjoon.silver.basicMath2.S_9020;
 
 import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    S_4948 test = new S_4948();
+    S_9020 test = new S_9020();
     test.test();
   }
 }
