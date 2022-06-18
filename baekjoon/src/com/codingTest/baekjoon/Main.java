3481e1e10341193e466b165feda9fa06f356e3d6
Jjiya <gpwl9610@naver.com>
2022-06-18 23:25:48 +0900

    sorting 2750 / memory Exceeded 2447

--- baekjoon/src/com/codingTest/baekjoon/Main.java
+++ baekjoon/src/com/codingTest/baekjoon/Main.java
@@ -1,12 +1,13 @@
 package com.codingTest.baekjoon;
 
-import com.codingTest.baekjoon.bronze.bruteForce.B_2798;
+
+import com.codingTest.baekjoon.bronze.sorting.B_2750;
 
 import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    B_2798 test = new B_2798();
+    B_2750 test = new B_2750();
     test.test();
   }
 }
