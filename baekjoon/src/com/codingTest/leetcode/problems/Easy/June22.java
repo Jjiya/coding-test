057def43f879740c6713a42f23bbd343e6f2bb9d
Jjiya <gpwl9610@naver.com>
2022-06-19 13:45:46 +0900

    solved 13. Roman to Integer

--- baekjoon/src/com/codingTest/leetcode/problems/Easy/June22.java
+++ baekjoon/src/com/codingTest/leetcode/problems/Easy/June22.java
@@ -3,6 +3,40 @@ package com.codingTest.leetcode.problems.Easy;
 import java.util.HashMap;
 
 public class June22 {
+  //  https://leetcode.com/problems/roman-to-integer/
+  public int romanToInt(String s) {
+    HashMap<String, Integer> romanNumerals = new HashMap<>();
+    romanNumerals.put("I", 1);
+    romanNumerals.put("V", 5);
+    romanNumerals.put("X", 10);
+    romanNumerals.put("L", 50);
+    romanNumerals.put("C", 100);
+    romanNumerals.put("D", 500);
+    romanNumerals.put("M", 1000);
+
+    String[] chars = s.split("");
+
+    int answer = 0;
+    int[] numeric = new int[chars.length];
+    for (int i = 0; i < chars.length; i++) {
+      numeric[i] = romanNumerals.get(chars[i]);
+    }
+
+    int sum = 0;
+    for (int j = 0; j < numeric.length; j++) {
+      if (j != numeric.length - 1) {
+        int division = numeric[j + 1] / numeric[j];
+        if (division == 5 || division == 10) {
+          sum += (numeric[j + 1] - numeric[j]);
+          j++;
+          continue;
+        }
+      }
+      sum += numeric[j];
+    }
+
+    return sum;
+  }
 
   public boolean isPalindrome1(int x) {
     if (x < 0) return false;
