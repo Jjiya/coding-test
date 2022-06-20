746f5db8d0ed6c73a608e06637021ad64c4a2832
Jjiya <gpwl9610@naver.com>
2022-06-20 13:55:41 +0900

    bruteForce silver 7568

--- baekjoon/src/com/codingTest/baekjoon/Main.java
+++ baekjoon/src/com/codingTest/baekjoon/Main.java
@@ -1,12 +1,11 @@
 package com.codingTest.baekjoon;
 
-import com.codingTest.baekjoon.bronze.string.B_1152;
+import com.codingTest.baekjoon.silver.bruteForce.S_7568;
 
 import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    B_1152 test = new B_1152();
-    test.test();
+    new S_7568().test();
   }
 }
