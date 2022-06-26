e1e0a3ac4c9a5e9d1705ed525ed1bdc4473ffd97
Jjiya <gpwl9610@naver.com>
2022-06-26 16:24:19 +0900

    yonsei university open contest solved a, b. c is solving. c have occur runtime Exception.

---
+++ baekjoon/src/com/codingTest/baekjoon/yonseiUnivOpenContest/A_25304.java
@@ -0,0 +1,20 @@
+package com.codingTest.baekjoon.yonseiUnivOpenContest;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class A_25304 {
+  public void test() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int total = Integer.parseInt(br.readLine());
+    int count = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < count; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      total -= (Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken()));
+      if (total < 0) break;
+    }
+
+    System.out.print(total == 0 ? "Yes" : "No");
+  }
+}
---
+++ baekjoon/src/com/codingTest/baekjoon/yonseiUnivOpenContest/B_25305.java
@@ -0,0 +1,32 @@
+package com.codingTest.baekjoon.yonseiUnivOpenContest;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class B_25305 {
+  public void test() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int k = Integer.parseInt(st.nextToken());
+
+    int[] scores = new int[n];
+
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      scores[i] = Integer.parseInt(st.nextToken());
+    }
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < i; j++) {
+        if (scores[i] > scores[j]) {
+          int temp = scores[i];
+          scores[i] = scores[j];
+          scores[j] = temp;
+        }
+      }
+    }
+
+    System.out.print(scores[k - 1]);
+  }
+}
