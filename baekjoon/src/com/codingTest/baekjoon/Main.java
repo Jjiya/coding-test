f534fa752e351576d4cf31a3ccbc4ea44d48dc95
Jjiya <gpwl9610@naver.com>
2022-07-06 16:30:22 +0900

    solving string bronze B_2743

--- baekjoon/src/com/codingTest/baekjoon/Main.java
+++ baekjoon/src/com/codingTest/baekjoon/Main.java
@@ -1,12 +1,12 @@
 package com.codingTest.baekjoon;
 
 import com.codingTest.baekjoon.bronze.greedy.*;
-import com.codingTest.baekjoon.bronze.string.B_10953;
+import com.codingTest.baekjoon.bronze.string.*;
 
 import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    new B_10953().test();
+    new B_2743().test();
   }
 }
---
+++ baekjoon/src/com/codingTest/baekjoon/bronze/string/B_2743.java
@@ -0,0 +1,9 @@
+package com.codingTest.baekjoon.bronze.string;
+
+import java.io.*;
+
+public class B_2743 {
+  public void test() throws IOException {
+    System.out.print(new BufferedReader(new InputStreamReader(System.in)).readLine().length());
+  }
+}
