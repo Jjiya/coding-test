7ecd90c8056301733f19e89158fed9c4270e7aaf
Jjiya <gpwl9610@naver.com>
2022-07-18 00:36:27 +0900

    solving 숫자 문자열과 영단어(81301)

--- programmers/src/ProgrammersMain.java
+++ programmers/src/ProgrammersMain.java
@@ -1,7 +1,7 @@
-import level_1.July_2;
+import level_1.July_3;
 
 public class ProgrammersMain {
   public static void main(String[] args) {
-    new July_2().solution("........................");
+    new July_3().solution("23four5six7");
   }
 }
