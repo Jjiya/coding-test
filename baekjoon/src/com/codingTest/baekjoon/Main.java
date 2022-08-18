62f3115149c35b5cb0bacc8ae2d220dfa9ae2f01
Jjiya <gpwl9610@naver.com>
2022-08-18 23:03:13 +0900

    solving math 2609

--- baekjoon/src/com/codingTest/baekjoon/Main.java
+++ baekjoon/src/com/codingTest/baekjoon/Main.java
@@ -1,24 +1,11 @@
 package com.codingTest.baekjoon;
 
 import com.codingTest.baekjoon.bronze.basicMath1.*;
-import com.codingTest.baekjoon.bronze.greedy.*;
-import com.codingTest.baekjoon.bronze.implement.*;
-import com.codingTest.baekjoon.bronze.io.*;
-import com.codingTest.baekjoon.bronze.sorting.*;
-import com.codingTest.baekjoon.bronze.string.*;
-import com.codingTest.baekjoon.silver.dataStructure.*;
-import com.codingTest.baekjoon.silver.dynamicProgramming.*;
-import com.codingTest.baekjoon.silver.graph.*;
-import com.codingTest.baekjoon.silver.greedy.*;
-import com.codingTest.baekjoon.silver.implement.*;
-import com.codingTest.baekjoon.silver.recursive.*;
-import com.codingTest.baekjoon.silver.sorting.*;
-import com.codingTest.baekjoon.silver.string.*;
 
 import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    new S_1181().test();
+    new B_2609().test();
   }
 }
