d19f225b50f60e9c7ebfada5f07522c99384d9d6
Jjiya <gpwl9610@naver.com>
2022-09-22 04:22:52 +0900

    solving math 2455

---
+++ baekjoon/src/com/codingTest/baekjoon/bronze/basicMath1/B_2455.java
@@ -0,0 +1,25 @@
+package com.codingTest.baekjoon.bronze.basicMath1;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class B_2455 {
+  public void test() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    int currentCount = 0;
+    int maxCount = 0;
+
+    for (int i = 0; i < 4; i++) {
+      st = new StringTokenizer(br.readLine());
+      int outCount = Integer.parseInt(st.nextToken());
+      int inCount = Integer.parseInt(st.nextToken());
+
+      maxCount = Math.max(maxCount, (currentCount + inCount - outCount));
+      currentCount += inCount - outCount;
+    }
+
+    System.out.print(maxCount);
+  }
+}
