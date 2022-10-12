8bc74ca3dad4ca05af2812fab396522b669b7a02
Jjiya <gpwl9610@naver.com>
2022-10-12 19:49:56 +0900

    solving math 2506

--- baekjoon/src/com/codingTest/baekjoon/Main.java
+++ baekjoon/src/com/codingTest/baekjoon/Main.java
@@ -1,7 +1,7 @@
 package com.codingTest.baekjoon;
 
-import com.codingTest.baekjoon.bronze.basicMath1.B_10886;
-import com.codingTest.baekjoon.bronze.greedy.B_4796;
+import com.codingTest.baekjoon.bronze.basicMath1.*;
+import com.codingTest.baekjoon.bronze.greedy.*;
 import com.codingTest.baekjoon.bronze.string.*;
 import com.codingTest.baekjoon.silver.greedy.S_16953;
 
@@ -9,6 +9,6 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    new S_16953().test();
+    new B_2506().test();
   }
 }
