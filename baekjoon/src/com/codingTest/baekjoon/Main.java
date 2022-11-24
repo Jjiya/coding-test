97b7ca95ba9b096fdcb768cf9a6010df666984e9
Jjiya <gpwl9610@naver.com>
2022-11-25 02:30:11 +0900

    solving first gold level! data structure 9935
    시간 효율은 안좋긴 함...
    memory 	120,764 KB
    시간 880 ms

--- baekjoon/src/com/codingTest/baekjoon/Main.java
+++ baekjoon/src/com/codingTest/baekjoon/Main.java
@@ -1,6 +1,7 @@
 package com.codingTest.baekjoon;
 
 import com.codingTest.baekjoon.bronze.string.B_2902;
+import com.codingTest.baekjoon.gold.dataStructure.G_9935;
 import com.codingTest.baekjoon.silver.backTracking.*;
 import com.codingTest.baekjoon.silver.dynamicProgramming.S_14501;
 import com.codingTest.baekjoon.silver.graph.S_1012;
@@ -10,6 +11,6 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    new S_1012().test();
+    new G_9935().test();
   }
 }
--- baekjoon/src/com/codingTest/baekjoon/gold/dataStructure/G_9935.java
+++ baekjoon/src/com/codingTest/baekjoon/gold/dataStructure/G_9935.java
@@ -5,26 +5,51 @@ import java.util.Stack;
 
 public class G_9935 {
   public void test() throws IOException {
-//    음...ㅎㅎ
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String str = br.readLine();
     String bomb = br.readLine();
 
     Stack<Character> bombResult = new Stack<>();
 
-    StringBuilder sbStack = new StringBuilder();
     for (int i = 0; i < str.length(); i++) {
-      sbStack.append(str.charAt(i));
-      if (sbStack.length() == bomb.length()) {
-        if (sbStack.toString().equals(bomb)) {
-          
-        } else {
+      char current = str.charAt(i);
 
+      if (bombResult.isEmpty()) {
+        if (!(bomb.length() == 1 && bomb.equals(String.valueOf(current)))) {
+          bombResult.push(current);
         }
+      } else if (current == bomb.charAt(bomb.length() - 1) && bombResult.size() >= bomb.length() - 1) {
+//        현재 글자가 폭발 문자열의 마지막 문자와 동일할 때
+        StringBuilder bombSb = new StringBuilder();
+        bombSb.append(current);
+
+        Stack<Character> subStack = new Stack<>();
+
+        for (int j = 0; j < bomb.length() - 1; j++) {
+          char getOne = bombResult.pop();
+          subStack.push(getOne);
+          bombSb.append(getOne);
+        }
+
+        if (!bomb.equals(bombSb.reverse().toString())) {
+          while (!subStack.isEmpty()) {
+            bombResult.push(subStack.pop());
+          }
+          bombResult.push(current);
+        }
+      } else {
+        bombResult.push(current);
       }
     }
 
+    if (bombResult.isEmpty()) {
+      System.out.print("FRULA");
+    } else {
+      StringBuilder sb = new StringBuilder();
+      bombResult.forEach(sb::append);
 
+      System.out.print(sb);
+    }
   }
 
   //   시간 초과는 안나는데, 메모리 초과 남
