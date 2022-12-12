da42ac90ed35286d2dc06553714b14944abced52
Jjiya <gpwl9610@naver.com>
2022-12-12 22:31:42 +0900

    solving sorting 20126

--- baekjoon/src/com/codingTest/baekjoon/Main.java
+++ baekjoon/src/com/codingTest/baekjoon/Main.java
@@ -7,12 +7,13 @@ import com.codingTest.baekjoon.gold.graph.G_7576;
 import com.codingTest.baekjoon.silver.backTracking.*;
 import com.codingTest.baekjoon.silver.dynamicProgramming.*;
 import com.codingTest.baekjoon.silver.graph.*;
+import com.codingTest.baekjoon.silver.sorting.S_20126;
 import com.codingTest.baekjoon.silver.tree.*;
 
 import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    new B_1032().test();
+    new S_20126().test();
   }
 }
