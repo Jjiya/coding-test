ff1216c34ee694792d40020720b9a513a982c8f7
Jjiya <gpwl9610@naver.com>
2022-12-15 22:22:13 +0900

    solving dp 11060

--- baekjoon/src/com/codingTest/baekjoon/Main.java
+++ baekjoon/src/com/codingTest/baekjoon/Main.java
@@ -1,19 +1,11 @@
 package com.codingTest.baekjoon;
 
-import com.codingTest.baekjoon.bronze.dynamicProgramming.B_24416;
-import com.codingTest.baekjoon.bronze.implement.B_1032;
-import com.codingTest.baekjoon.gold.graph.G_14502;
-import com.codingTest.baekjoon.gold.graph.G_7576;
-import com.codingTest.baekjoon.silver.backTracking.*;
 import com.codingTest.baekjoon.silver.dynamicProgramming.*;
-import com.codingTest.baekjoon.silver.graph.*;
-import com.codingTest.baekjoon.silver.sorting.S_20126;
-import com.codingTest.baekjoon.silver.tree.*;
 
 import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    new S_2583().test();
+    new S_11060().test();
   }
 }
