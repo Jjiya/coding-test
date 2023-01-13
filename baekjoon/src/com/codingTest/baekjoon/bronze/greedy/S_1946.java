17c12fa4ba44d5aa4ddfe2dfd63ae703b58ab6bc
Jjiya <gpwl9610@naver.com>
2023-01-14 00:47:52 +0900

    add solving greedy 1946

--- baekjoon/src/com/codingTest/baekjoon/bronze/greedy/S_1946.java
+++ baekjoon/src/com/codingTest/baekjoon/bronze/greedy/S_1946.java
@@ -6,7 +6,44 @@ import java.util.StringTokenizer;
 
 public class S_1946 {
   // https://www.acmicpc.net/problem/1946
+
+  //  참고: https://www.acmicpc.net/source/39455094
+  //  아래와 같은 방법으로 수행 시 사원을 입력 받을 때부터 순위를 정해서 입력을 넣기 떄문에 중간에 정렬을 따로 할 필요가 없음!!
   public static void test() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int t = Integer.parseInt(br.readLine());
+
+    int[] list;
+    StringTokenizer st;
+
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < t; i++) {
+      int n = Integer.parseInt(br.readLine());
+
+      list = new int[n + 1];
+
+      for (int j = 0; j < n; j++) {
+        st = new StringTokenizer(br.readLine());
+
+        list[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
+      }
+
+      int maxGrade = list[1], count = 1;  // count에 1위 포함
+      for (int j = 2; j <= n; j++) {
+        if (list[j] < maxGrade) {
+          count++;
+          maxGrade = list[j];
+        }
+      }
+
+      sb.append(count).append("\n");
+    }
+
+    System.out.print(sb);
+  }
+
+  public static void test1() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int t = Integer.parseInt(br.readLine());
 
@@ -131,4 +168,19 @@ public class S_1946 {
         => 이 경우에 3 3 지원자는 다른 2 2 지원자보다 더 높은 성적이 없으므로 제외, 3명이 답이어야 한다고 함
 
         그럼 1등을 보유한 지원자를 제외하고는, 모든 지원자를 순회하며 뛰어난게 있으면 뽑아야겠네...
-       */
+
+        ==> 최대 지원자 수는 10만 명이기 때문에 2중 for문 돌리면 시간 초과 발생
+        한 개의 항목(A)에 대해 오름차순으로 정렬 후 다른 항목(B)에 대해 순위를 비교하여야 한다.
+        A B
+        1 4
+        2 2
+        3 3
+        4 1
+        와 같은 경우 A에 대해 오름차순으로 정렬하였고,
+        이후 1위부터 B에 대한 최소값을 측정하며, 해당 최소 값보다 작은 경우에만 신입 사원으로 선발한다.
+        3 3 사원의 경우 B에 대한 순위가 앞의 2 2 사원보다 낮기 때문에 선발될 수 없다.
+
+        이렇게 정의가 가능한 이유는 A에 대해 이미 순위별로 정렬을 했기 때문이다.
+        앞의 사원들은 정렬된 A에 대해서는 무조건 뒤의 사원들 보다 높은 점수를 가졌음을 보장할 수 있기 때문...
+
+      원*/
