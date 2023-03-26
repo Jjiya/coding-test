362794b430a43f81c36af2ef2a822a11c4f6aa51
Jjiya <66312330+Jjiya@users.noreply.github.com>
2023-03-26 14:26:10 +0900

    chore: 코멘트 추가

--- baekjoon/src/com/codingTest/baekjoon/bronze/implement/B_10797.java
+++ baekjoon/src/com/codingTest/baekjoon/bronze/implement/B_10797.java
@@ -3,6 +3,10 @@ package com.codingTest.baekjoon.bronze.implement;
 import java.io.*;
 import java.util.StringTokenizer;
 
+/* 원래 2023 Mar 24일 제출용이였으나
+ * 너무 피곤해서 그랬는지 제출을 안하고 commit만 함...
+ * 그래서 boj에 제츨은 26일에 한것으로 되었음 ㅠㅠ
+ */
 public class B_10797 {
   // https://www.acmicpc.net/problem/10797
   public static void test() throws IOException {
